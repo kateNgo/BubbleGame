baabaa150d5941d910e4093f945b135f6a46131d
phuongngovn <phuong@andres-air>
2017-05-01 13:00:47 +1000

    Initial Commit

---
+++ MyGame2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyGame2
+//
+//  Created by phuong on 1/5/17.
+//  Copyright © 2017 ppben. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
